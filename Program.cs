99fd74a7ed62bb03ba393addccb6a4dd3030bdd8
Luca Donzelli <luca.donzelli.ld98@gmail.com>
2023-01-25 13:47:55 +0100

    Written a simple main for testing
    First try let's goo
    The 'Dict' class is written correctly atm
    Both upload and filter works as they should

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public static class SharpWordleSolver
+{
+    static void Main(string[] args)
+    {
+        if (args.Length <= 0)
+        {
+            Console.WriteLine("Please launch the application with the path to the relevant dictionary");
+            Environment.Exit(0); // Not sure about this
+        }
+        Dict dictionary = new Dict(args[0]);
+        // Writing the rest of the main later
+        // This one is just a test to see if I'm writing the dictionary class correctly
+        foreach(string word in dictionary.Words)
+        {
+            Console.WriteLine(word);
+        }
+        dictionary.filterDictionary("^[b][a-z][a-z][a-z][a-z]$");
+        Console.WriteLine("\n\n\nTrying the filter test now");
+        foreach(string word in dictionary.Words)
+        {
+            Console.WriteLine(word);
+        }
+    }
+}
