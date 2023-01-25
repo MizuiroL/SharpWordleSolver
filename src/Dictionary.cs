ee147869b660fda6ebfd1f66f22e801d05f1c2ae
Luca Donzelli <luca.donzelli.ld98@gmail.com>
2023-01-25 13:19:38 +0100

    Added 'uploadWords' method
    Read the txt file containing all the words and put that into the Words list

--- src/Dictionary.cs
+++ src/Dictionary.cs
@@ -9,6 +9,12 @@ public class Dict
 
     public IList<string> Words { get; set; }
 
+    public void uploadWords(string path)
+    {
+        string[] lines = System.IO.File.ReadAllLines(path);
+        Words = lines.ToList<string>();
+    }
+
     /* Both updates the 'Words' List and returns this new list
        Not sure about this implementation yet */
     public IList<string> filterDictionary(string filter)
