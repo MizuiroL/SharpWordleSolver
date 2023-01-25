b081708d4e1b221dcc33d95bfd690a34d6351bac
Luca Donzelli <luca.donzelli.ld98@gmail.com>
2023-01-25 13:10:57 +0100

    Initial commit

---
+++ src/Dictionary.cs
@@ -0,0 +1,27 @@
+using System.Text.RegularExpressions;
+
+public class Dict
+{
+    public Dict()
+    {
+        Words = new List<string>();
+    }
+
+    public IList<string> Words { get; set; }
+
+    /* Both updates the 'Words' List and returns this new list
+       Not sure about this implementation yet */
+    public IList<string> filterDictionary(string filter)
+    {
+        List<string> newList = new List<string>();
+        foreach(string word in Words)
+        {
+            if(Regex.Match(word, filter).Success)
+            {
+                newList.Add(word);
+            }
+        }
+        Words = newList;
+        return newList;
+    }
+}
