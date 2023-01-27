cbe9f6b48d0f19f0eef2621b6dbadc17568f961e
Luca Donzelli <luca.donzelli.ld98@gmail.com>
2023-01-27 14:40:17 +0100

    First partially working version

--- Program.cs
+++ Program.cs
@@ -8,9 +8,10 @@
             Environment.Exit(0); // Not sure about this
         }
         Dict dictionary = new Dict(args[0]);
+        dictionary.print();
         // Writing the rest of the main later
         // This one is just a test to see if I'm writing the dictionary class correctly
-        foreach(string word in dictionary.Words)
+        /*foreach(string word in dictionary.Words)
         {
             Console.WriteLine(word);
         }
@@ -19,6 +20,29 @@
         foreach(string word in dictionary.Words)
         {
             Console.WriteLine(word);
-        }
+        }*/
+
+        string word = "";
+        string output = "";
+        int[] code = {0,0,0,0,0};
+        do
+        {
+            word = Console.ReadLine().ToLower();
+            output = Console.ReadLine().ToLower();
+            if(InputManager.checkWordValidity(word) && InputManager.checkCodeValidity(output))
+            {
+                Console.WriteLine("IMIN");
+                code = InputManager.codify(output);
+                RegexBuilder rb = new RegexBuilder(word, code);
+                foreach(var regex in rb.RegexList)
+                {
+                    dictionary.filterDictionary(regex);
+                }
+                dictionary.print();
+            } else
+            {
+                Console.WriteLine("Please input a valid word and/or code\n5 LETTER WORD\n[0-2][0-2][0-2][0-2][0-2]");
+            }
+        } while(dictionary.count() > 1);
     }
 }
